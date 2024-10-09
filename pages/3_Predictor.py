478cc9862f3e55314823a76758c3a3d2eb0213b5
aditya-somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-10 00:46:36 +0530

    Update 3_Predictor.py

--- pages/3_Predictor.py
+++ pages/3_Predictor.py
@@ -13,14 +13,14 @@ if __name__ == '__main__':
         st.title("Stock Predictor 📐")
         st.write('This page gives insights into the possible price values of a stock')
 
-        ip_file = 'config/Indices.csv'
-        index_frame = pd.read_csv(ip_file, sep='|')
-        index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
+        # ip_file = 'config/Indices.csv'
+        # index_frame = pd.read_csv(ip_file, sep='|')
+        # index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
 
         ########### Display initial options----------
         col1, col2, col3 = st.columns([1, 0.75, 1])
-        with col1:
-            symbol_select_option = st.selectbox('Choose a Symbol:', sorted(list(index_frame['Options'])), index=0)
+        # with col1:
+        #     symbol_select_option = st.selectbox('Choose a Symbol:', sorted(list(index_frame['Options'])), index=0)
         with col3:
             user_cookie = st.text_input("Cookie Value from NSE", 
                                         help="""To get Cookie value, go to the NSE website and look to download a CSV file of any symbol. The API call that gets triggered will have a cookie that can be accessed via Developer tools from a web browser.""")
