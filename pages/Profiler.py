fccff29f8a6b77c25700d8b8a180d3690ff5beee
aditya-somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-09 23:13:50 +0530

    Update Profiler.py

--- pages/Profiler.py
+++ pages/Profiler.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
     try:
         
         ########### Set page title and icon----------
-        st.set_page_config(page_title="Stocker - Profiler", page_icon="eyeglasses", layout="wide")
+        st.set_page_config(page_title="Stock Profiler", page_icon="eyeglasses", layout="wide")
         
         ########### Display Page Title----------
         # st.sidebar.markdown("# Main Page")
-        st.title("Profiler")
-        st.write('This page gives insights into the profiling analysis - both fundamental and technical of the individual stock')
+        st.title("Stock Profiler")
+        st.write('This page gives insights into the profiling analysis - both fundamental and technical, of the individual stock')
         st.markdown("---")
         
     except Exception as e:
