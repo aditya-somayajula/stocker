7dc9b075fdd83af15a3510268482ebe206daae82
aditya-somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-09 23:05:56 +0530

    Create Profiler.py
    Added new Page - Profiler

---
+++ pages/Profiler.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    try:
+        
+        ########### Set page title and icon----------
+        st.set_page_config(page_title="Stocker - Profiler", page_icon="eyeglasses", layout="wide")
+        
+        ########### Display Page Title----------
+        # st.sidebar.markdown("# Main Page")
+        st.title("Profiler")
+        st.write('This page gives insights into the profiling analysis - both fundamental and technical of the individual stock')
+        st.markdown("---")
+        
+    except Exception as e:
+        # logging.error(e)
+        print(e)
+        sys.exit(0)
+      
