7db25de2ac39b092bbb574700f88416d765833b8
aditya-somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-09 23:22:22 +0530

    Create 3_Predictor.py

---
+++ pages/3_Predictor.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import sys
+
+if __name__ == '__main__':
+    try:
+        
+        ########### Set page title and icon----------
+        st.set_page_config(page_title="Stock Predictor", page_icon="triangular_ruler", layout="wide")
+        
+        ########### Display Page Title----------
+        # st.sidebar.markdown("# Main Page")
+        st.title("Stock Predictor")
+        st.write('This page gives insights into the possible price values of a stock')
+        st.markdown("---")
+        
+    except Exception as e:
+        # logging.error(e)
+        print(e)
+        sys.exit(0)
+      
