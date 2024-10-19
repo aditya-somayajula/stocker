41b92bd927086e64c491078a8cee98b2a98fac72
Aditya Somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-19 13:11:45 +0530

    Update 3_Predictor.py

--- pages/3_Predictor.py
+++ pages/3_Predictor.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 
         ip_file = 'config/Indices.csv'
         index_frame = pd.read_csv(ip_file, sep='|')
+        index_frame = index_frame[['SYMBOL', 'COMPANY']].drop_duplicates(subset=['SYMBOL', 'COMPANY'], keep='first')
         index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
 
         ########### Display initial options----------
