b8f32971df8e9b1580a6b38403604a0084854ae3
Aditya Somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-19 13:01:21 +0530

    Update 1_Profiler.py

--- pages/1_Profiler.py
+++ pages/1_Profiler.py
@@ -6,26 +6,37 @@ if __name__ == '__main__':
     try:
         
         ########### Set page title and icon----------
-        st.set_page_config(page_title="Stock Profiler", page_icon="eyeglasses", layout="wide")
+        st.set_page_config(page_title='Stock Profiler', page_icon='eyeglasses', layout='wide')
         
         ########### Display Page Title----------
         # st.sidebar.markdown("# Main Page")
-        st.title("Stock Profiler 👓")
-        st.write('This page gives insights into the profiling analysis - both fundamental and technical, of the individual stock')
+        st.title('Stock Profiler 👓')
+        st.write('This page gives insights into the profiling analysis - both fundamental and technical, of the individual stock.')
 
         ip_file = 'config/Indices.csv'
         index_frame = pd.read_csv(ip_file, sep='|')
+        index_frame = index_frame[['SYMBOL', 'COMPANY']].drop_duplicates(subset=['SYMBOL', 'COMPANY'], keep='first')
         index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
 
         ########### Display initial options----------
         col1, col2, col3 = st.columns([1, 0.75, 1])
         with col1:
-            symbol_select_option = st.selectbox('Choose a Symbol:', sorted(list(index_frame['Options'])), index=0)
+            symbol_select_option = st.selectbox('***Please select a Symbol***', sorted(list(index_frame['Options'])), index=None, placeholder='Choose a Symbol...')
         with col3:
-            user_cookie = st.text_input("Cookie Value from NSE", 
-                                        help="""To get Cookie value, go to the NSE website and look to download a CSV file of any symbol. The API call that gets triggered will have a cookie that can be accessed via Developer tools from a web browser.""")
+            user_cookie = st.text_input('***Cookie Value from NSE***', 
+                                        help='To get Cookie value, go to the NSE website and look to download a CSV file of any symbol. The API call that gets triggered will have a cookie that can be accessed via Developer tools from a web browser')
         
-        st.markdown("---")
+        
+        analysis = st.radio('***Please select analysis type***', ['Fundamental', 'Technical', 'Fundamental & Technical'], index=None,)
+        result = st.button('Run Profiling', type='secondary')
+        
+        st.markdown('---')
+        
+        ########### Display Profiling Results---------- 
+        if symbol_select_option is not None and analysis is not None and result:
+            st.write(f'Thank you for selecting {symbol_select_option} for {analysis} analysis. I\'m coming pretty soon.')
+        else:
+            st.write('Please make a selection...!!!')
         
     except Exception as e:
         # logging.error(e)
