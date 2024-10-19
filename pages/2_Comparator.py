25b8fc2d1b5514eec31b4f7d22c95e3ad2372299
Aditya Somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-19 14:06:24 +0530

    Update 2_Comparator.py

--- pages/2_Comparator.py
+++ pages/2_Comparator.py
@@ -13,21 +13,22 @@ if __name__ == '__main__':
         st.title('Stock Comparator 📊')
         st.write('This page gives insights into the comparision analysis between 2 or more stocks')
         
+        ########### Get Values----------
         ip_file = 'config/Indices.csv'
         index_frame = pd.read_csv(ip_file, sep='|')
         index_frame = index_frame[['SYMBOL', 'COMPANY']].drop_duplicates(subset=['SYMBOL', 'COMPANY'], keep='first')
         index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
 
         ########### Display initial options----------
-        col1, col2, col3 = st.columns([1, 0.75, 1])
+        col1, col2, col3 = st.columns([1, 1, 1])
         with col1:
             symbol_select_option = st.multiselect('***Choose a Symbol(s)***', sorted(list(index_frame['Options'])))
         with col3:
             user_cookie = st.text_input("***Cookie Value from NSE***", 
                                         help='To get Cookie value, go to the NSE website and look to download a CSV file of any symbol. The API call that gets triggered will have a cookie that can be accessed via Developer tools from a web browser')
+        st.write('')
         
         result = st.button('Run Comparision', type='secondary')
-        
         st.markdown('---')
         
         ########### Display Comparision Results----------        
