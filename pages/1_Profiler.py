c0dbb593f393617afaaf5b82b790f41d64f4b53e
Aditya Somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-24 22:47:00 +0530

    Update 1_Profiler.py

--- pages/1_Profiler.py
+++ pages/1_Profiler.py
@@ -8,16 +8,19 @@ if __name__ == '__main__':
         ########### Set page title and icon----------
         st.set_page_config(page_title='Stock Profiler', page_icon='eyeglasses', layout='wide')
         
+        
         ########### Display Page Title----------
         # st.sidebar.markdown("# Main Page")
         st.title('Stock Profiler 👓')
         st.write('This page gives insights into the profiling analysis - both fundamental and technical, of the individual stock.')
+
 
         ########### Get Values----------
         ip_file = 'config/Indices.csv'
         index_frame = pd.read_csv(ip_file, sep='|')
         index_frame = index_frame[['SYMBOL', 'COMPANY']].drop_duplicates(subset=['SYMBOL', 'COMPANY'], keep='first')
         index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
+
 
         ########### Display initial options----------
         col1, col2, col3 = st.columns([1, 1, 1])
@@ -32,11 +35,18 @@ if __name__ == '__main__':
         result = st.button('Run Profiling', type='secondary')
         st.markdown('---')
         
+        
         ########### Display Profiling Results---------- 
         if symbol_select_option is not None and analysis is not None and result:
             st.write(f'Thank you for selecting {symbol_select_option} for {analysis} analysis. I\'m coming pretty soon.')
         else:
             st.write('Please make a selection...!!!')
+        
+        ########### Display Page Footer---------- 
+        footer = '''<div style='position:fixed;bottom:0;width:100%;background-color:transparent;text-align:center;padding:10px;font-size:16px;color:#ffffff;'>
+            © 2024 Stocker. All Rights Reserved. Contact <a style='color:#ffa500;' href="mailto:adityas.sai@gmail.com">adityas.sai@gmail.com</a> for more information.
+            </div>'''
+        st.markdown(footer, unsafe_allow_html=True)
         
     except Exception as e:
         # logging.error(e)
