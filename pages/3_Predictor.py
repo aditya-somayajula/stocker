6d8e78b67fc1a8f1c8058285e6d710c28670750b
Aditya Somayajula <151649302+aditya-somayajula@users.noreply.github.com>
2024-10-24 23:31:38 +0530

    Update 3_Predictor.py

--- pages/3_Predictor.py
+++ pages/3_Predictor.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import pandas as pd
 import sys
+import json
 import common_functions
 
 if __name__ == '__main__':
     try:
-        
+        with open('E:/PythonScripts/Stocker/config/config.json', encoding='utf-8') as cf:
+            config_data = json.load(cf)
+            
+            
         ########### Set page title and icon----------
         st.set_page_config(page_title='Stock Predictor', page_icon='triangular_ruler', layout='wide')
+        
         
         ########### Display Page Title----------
         # st.sidebar.markdown("# Main Page")
         st.title('Stock Predictor 📐')
         st.write('This page gives insights into the possible price values of a stock')
+
 
         ########### Get Values----------
         ip_file = 'config/Indices.csv'
@@ -20,8 +26,9 @@ if __name__ == '__main__':
         index_frame = index_frame[['SYMBOL', 'COMPANY']].drop_duplicates(subset=['SYMBOL', 'COMPANY'], keep='first')
         index_frame['Options'] = index_frame['SYMBOL'] + ' (' + index_frame['COMPANY'] + ')'
         
-        feature_Set_list = ['ALL', 'Feature Set 1', 'Feature Set 2', 'Feature Set 3', 'Feature Set 4', 'Feature Set 5', 'Feature Set 6']
-        model_list = ['ALL', 'Linear Regression', 'Decision Trees', 'Random Forest', 'XG Boost']
+        feature_Set_list = list(config_data['features'].keys())
+        model_list = list(config_data['models'].keys())
+
 
         ########### Display initial options----------
         col1, col2, col3 = st.columns([1, 1, 1])
@@ -34,13 +41,14 @@ if __name__ == '__main__':
         
         col1, col2,col3 = st.columns([1, 1, 1])
         with col1:
-            analysis = st.radio('***Choose a Prediction Type***', ['Active Prediction', 'Passiv Prediction'], index=None, captions=['***Predict prices during trading window***', '***Predict prices post trading window***'], horizontal=True)
+            analysis = st.radio('***Choose a Prediction Type***', ['Active Prediction', 'Passive Prediction'], index=None, captions=['***Predict prices during trading window***', '***Predict prices post trading window***'], horizontal=True)
         with col2:
             feature_select_option = st.selectbox('***Choose a Feature Set***', feature_Set_list, index=None)
         with col3:
             model_select_option = st.selectbox('***Choose a Model***', model_list, index=None)
         result = st.button('Run Prediction', type='secondary')
         st.markdown('---')
+        
         
         ########### Display Prediction Results----------
         if result:
@@ -56,18 +64,25 @@ if __name__ == '__main__':
                 st.warning('Please select a Model.')
             else:
                 if analysis == 'Active Prediction':
-                    mkt_status_bool, mkt_status_data = common_functions.get_market_status(user_cookie)
+                    mkt_status_bool, mkt_status_data, mkt_data = common_functions.get_market_status(user_cookie)
                     if mkt_status_bool:
-                        if mkt_status_data == 'Market is Closed':
+                        if mkt_status_data == 'Closed':
                             st.warning('Unable to perform Active Prediction as market is closed. Active Prediction is enabled only during trading window.')
-                        if mkt_status_data == 'Market is Open':
+                        if mkt_status_data == 'Open':
                             st.write('Active Prediction Code in progress. Stay Tuned...!!!')
                     else:
                         st.warning('Unable to get Market Status. Hence, unable to perform active prediction of prices. Contact administrator')
                 else:
-                    st.write('Passive Prediction Code in progress. Stay Tuned...!!!')
+                    common_functions.passive_predict(symbol_select_option.split(' (')[0], user_cookie, feature_select_option, model_select_option)
         else:
             st.write('Please make a selection to view predicted prices')
+        
+        
+        ########### Display Page Footer---------- 
+        footer = '''<div style='position:fixed;bottom:0;width:100%;background-color:transparent;text-align:center;padding:10px;font-size:16px;color:#ffffff;'>
+            © 2024 Stocker. All Rights Reserved. Contact <a style='color:#ffa500;' href="mailto:adityas.sai@gmail.com">adityas.sai@gmail.com</a> for more information.
+            </div>'''
+        st.markdown(footer, unsafe_allow_html=True)
             
     except Exception as e:
         # logging.error(e)
